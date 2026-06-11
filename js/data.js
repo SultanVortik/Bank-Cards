8fbdaea7fbc59ff25549b1393586337593433b04
magomedov_sultan <sultanvortik@gmail.com>
2026-06-11 23:02:46 +0300

    feat: add account page

---
+++ js/data.js
@@ -0,0 +1,21 @@
+const usersData = [
+    {
+        email: "user1@gmail.com",
+        password: "user1"
+    },
+    {
+        email: "user2@gmail.com",
+        password: "user2"
+    },
+    {
+        email: "user3@gmail.com",
+        password: "user3"
+    },
+    {
+        email: "user4@gmail.com",
+        password: "user4"
+    }
+
+]
+
+export default usersData
