4c6989c852f9c876c0c6a985ed4986d5bcff0209
magomedov_sultan <sultanvortik@gmail.com>
2026-06-22 00:02:00 +0300

    feat: add cards functional

--- js/addCards.js
+++ js/addCards.js
@@ -1,5 +1,6 @@
 const body = document.querySelector("body")
 const modalWindow = document.querySelector(".modal-window")
+const cardsStorage = document.querySelector(".cards-storage")
 const cardNumberInput = document.querySelector(".card-number-input")
 const selectPayment = document.querySelector(".payment-system")
 const addBtn = document.querySelector(".add-card-btn")
@@ -20,5 +21,22 @@ function openWindow(e) {
     body.classList.add("window-open")
 }
 
+function addCard(e) {
+    e.preventDefault()
+
+    if (cardNumberInput.value.length === 16) {
+        cardsStorage.innerHTML += `
+            <div class="card ${selectPayment.value}">
+                <span class="card__el title">${selectPayment.value}</span>
+                <span class="card__el">${cardNumberInput.value}</span>
+            </div>
+        `
+    } else {
+        window.alert("Номере карты должно быть 16 цифр")
+    }
+}
+
+
 closeBtn.addEventListener("click", closeWindow)
 modalWindowOpenBtn.addEventListener("click", openWindow)
+addBtn.addEventListener("click", addCard)
